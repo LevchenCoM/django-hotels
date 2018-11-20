1b05dd005b49e91723eaf0200bb8c2be022ddc5c
Mykola Levchenko <qupadlive@gmail.com>
2018-11-20 23:25:56 +0200

    mod

--- static/static_prod/js/custom.js
+++ static/static_prod/js/custom.js
@@ -30,6 +30,7 @@ $(document).ready(function() {
   var menu = $('.menu');
   var menuActive = false;
   var burger = $('.hamburger');
+  var map;
 
   setHeader();
 
@@ -51,8 +52,6 @@ $(document).ready(function() {
   initTestimonialsSlider();
   initScrolling();
   initInput();
-  initGoogleMap();
-
   /*
 
   2. Set Header
@@ -245,51 +244,4 @@ $(document).ready(function() {
   //   }
   //
   // };
-  function initGoogleMap()
-	{
-		var myLatlng = new google.maps.LatLng(34.063685,-118.272936);
-    	var mapOptions =
-    	{
-    		center: myLatlng,
-	       	zoom: 14,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: true,
-			scrollwheel: false,
-			zoomControl: true,
-			zoomControlOptions:
-			{
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: true,
-			styles:
-			[
-			  {
-			    "featureType": "road.highway",
-			    "elementType": "geometry.fill",
-			    "stylers": [
-			      {
-			        "color": "#ffeba1"
-			      }
-			    ]
-			  }
-			]
-    	}
-
-    	// Initialize a map with options
-    	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-		// Re-center map after window resize
-		google.maps.event.addDomListener(window, 'resize', function()
-		{
-			setTimeout(function()
-			{
-				google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatlng);
-			}, 1400);
-		});
-	}
 });
