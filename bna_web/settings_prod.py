f9076d369b7c9153163554460a5d1b00727f4e64
Mykola Levchenko <40539001+LevchenCoM@users.noreply.github.com>
2018-11-21 13:10:50 +0200

    Upgrade settings_prod

--- bna_web/settings_prod.py
+++ bna_web/settings_prod.py
@@ -4,9 +4,9 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bna_db',
-        'USER': 'bna_site',
-        'PASSWORD': 'MightyBogdan',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
