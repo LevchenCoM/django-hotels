61e52600a016a389a821adce24b17c005778a0bd
Pavel Tanchev <dcopm999@gmail.com>
2020-01-05 13:08:39 +0500

    bna_web.settings
    * Импорт настроек .settings_prod в случает DEBUG=False
    * SECRTET_KEY для DEBUG=True

--- bna_web/settings.py
+++ bna_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '123456'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -192,7 +192,8 @@ TINYMCE_DEFAULT_CONFIG = {
     'width': '1000'
 }
 
-try:
-    from .settings_prod import *
-except:
-    pass
+if not DEBUG:
+    try:
+        from .settings_prod import *
+    except:
+        pass
